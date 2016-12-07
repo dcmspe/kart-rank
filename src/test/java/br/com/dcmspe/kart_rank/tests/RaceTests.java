c77b0e97eca7bc0a95810c933d101e264d3c834a
Danilo Caetano <dcms.pe@gmail.com>
2016-12-06 22:42:40 -0200

    All tests are passing now!

--- src/test/java/br/com/dcmspe/kart_rank/tests/RaceTests.java
+++ src/test/java/br/com/dcmspe/kart_rank/tests/RaceTests.java
@@ -1,10 +1,13 @@
 package br.com.dcmspe.kart_rank.tests;
  
  import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.internal.runners.model.EachTestNotifier;
 
+import br.com.dcmspe.kart_rank.entities.Lap;
 import br.com.dcmspe.kart_rank.entities.Pilot;
 import br.com.dcmspe.kart_rank.entities.Race;
 import br.com.dcmspe.kart_rank.helpers.CSVObject;
@@ -28,11 +31,28 @@ import br.com.dcmspe.kart_rank.helpers.FileHelper;
  		List<CSVObject> csvObjects = FileHelper.readKartCSVFile();
  		Race race = new Race(csvObjects);
  		
+ 		List<Lap> lapsFromPilot = race.lapsFromPilot(race.getPilots().get("038"));
+ 		
+ 		Assert.assertTrue(lapsFromPilot.get(0).getPilot().getCode().equals("038"));
+ 		Assert.assertTrue(lapsFromPilot.get(1).getPilot().getCode().equals("038"));
+ 		Assert.assertTrue(lapsFromPilot.get(2).getPilot().getCode().equals("038"));
+ 	}
+ 	
+ 	@Test
+ 	public void should_get_laps_from_all_pilots(){
+ 		
+ 		List<CSVObject> csvObjects = FileHelper.readKartCSVFile();
+ 		Race race = new Race(csvObjects);
+ 		
+ 		Map<Pilot, List<Lap>> lapsFromPilots = race.listLapsFromPilots();
+ 		
  		for (Pilot pilot : race.getPilots().values()) {
-			System.out.println(pilot.getName());
+ 			List<Lap> laps = lapsFromPilots.get(pilot);
+ 			for (Lap lap : laps) {
+ 				Assert.assertTrue(lap.getPilot().getCode().equals(pilot.getCode()));
+			}
 		}
  		
- 		Assert.fail();
  	}
  	
  	 
